3d880cab17d11c43b845f874bb58e4f6904af031
allezz <aerbay@gmail.com>
2020-04-07 17:26:12 +0200

    Add Pin Check, Add Card Expire Check, Add Account Activity Check and all tests

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,7 +1,7 @@
 require './lib/atm.rb'
 require 'date'
 describe Atm do
-    let(:account) { instance_double('account') }
+    let(:account) { instance_double('Account',pin_code:"1234",exp_date:"04/22", account_status: :active) }
 
   before do 
         allow(account).to receive(:balance).and_return(100)
@@ -14,25 +14,43 @@ describe Atm do
   end
   
   it "funds are reduced at withdraw" do
-    subject.withdraw(50, account)
+    subject.withdraw(50,"1234", account)
     expect(subject.funds).to eq 950
   end
 
   it 'allow withdraw if the account has enough balance' do 
     expected_output = { status: true, message: 'success', date: Date.today, amount: 45}
-    expect(subject.withdraw(45, account)).to eq expected_output
+    expect(subject.withdraw(45,"1234", account)).to eq expected_output
 
   end
 
   it "rejects withdraw if account has insufficient funds" do
     expected_output={status:false,message:"insufficient funds", date:Date.today}
-    expect(subject.withdraw(105,account)).to eq expected_output
+    expect(subject.withdraw(105,"1234",account)).to eq expected_output
   end
   it 'reject withdraw if ATM has insufficient funds' do 
     subject.funds = 50
     expected_output = { status: false, message: 'insufficient funds in ATM', date: Date.today}
-    expect(subject.withdraw(100, account)).to eq expected_output
-end
+    expect(subject.withdraw(100,"1234", account)).to eq expected_output
+  end
+
+   it "reject withdraw if the pin is wrong" do
+    expected_output = { status:false, message:"wrong pin",date:Date.today}
+    expect(subject.withdraw(50,"9999",account)).to eq expected_output
+   end
+
+   it "reject withdraw if the card is expired" do
+    allow(account).to receive(:exp_date).and_return("12/17")
+    expected_output = {status:false,message:"card expired",date:Date.today}
+    expect(subject.withdraw(10,"1234",account)).to eq expected_output
+   end
+
+   it "reject withdraw if the account is disabled" do
+    allow(account).to receive(:account_status).and_return(:disabled)
+    expected_output = { status:false, message:"account is disabled",date:Date.today}
+    expect(subject.withdraw(100,"1234",account)).to eq expected_output
+   end
+
 
 end
 
