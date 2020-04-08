4633a66b62fa57892fc07fbdaa60543f2a5e5dd4
pauline_barnades <barnades.pauline@gmail.com>
2020-04-08 10:50:39 +0200

    Create method status and owner

--- lib/account.rb
+++ lib/account.rb
@@ -2,11 +2,28 @@ class Account
 
     STANDARD_VALIDITY_YRS = 5
 
+    def initialize(attrs = {})
+         @account_status = :active
+         set_owner(attrs[:owner])
+    end
+
+    def deactivate
+        @account_status = :deactivated
+
+    end
+    
     def set_expire_date
 
         Date.today.next_year(STANDARD_VALIDITY_YRS).strft("%m/%y")
         
     end
 
+private
+def set_owner(obj)
+    obj == nil ? missing_owner : @owner = obj
+end
 
+def missing_owner
+    raise "An Account owner is required"
+end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,6 +2,16 @@ require './lib/account.rb'
 require 'date'
 
 describe Account do
+    let(:person) {instance_double('Person', name: 'Thomas')}
+    subject { described_class.new({owner: person}) }
+
+    it "is expected to have an owner" do
+        expect(subject.owner).to eq person  
+    end
+
+    it "is expected to raise error if no owner is set" do 
+        expect { described_class.new }.to raise_error 'An account owner is required'
+    end
     
    it "check length of a number" do
        number=rand(1000..9999)
@@ -14,5 +24,14 @@ describe Account do
        expect(subject.exp_date).to eq expected_date
    end
 
+   it "expected to have : active status on initialize" do 
+    expect(subject.account_status).to eq :active 
+
+end
+    it "deactivates account using the instance method" do
+        subject.deactivate
+        expect(subject.account_status).to eq :deactivated 
+    
+    end
 
 end
