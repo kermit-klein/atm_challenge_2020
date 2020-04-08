12006400e373587d7c21cdff238e862486d4fe9c
pauline_barnades <barnades.pauline@gmail.com>
2020-04-08 16:38:46 +0200

    account ownership test

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -28,7 +28,10 @@ describe Person do
         it 'of Account class ' do
           expect(subject.account).to be_an_instance_of Account
         end
-    
+        
+    it 'with himself as an owner' do
+            expect(subject.account.owner).to be subject
+          end
         
       end
     end
